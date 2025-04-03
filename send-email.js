1307d94359545a29919697b7931a09b0a2dc1712
LFound <luke.found@icloud.com>
2025-04-03 20:05:16 +0100

    Update send-email.js

--- send-email.js
+++ send-email.js
@@ -13,13 +13,13 @@ const transporter = nodemailer.createTransport({
   port: 587,
   secure: false, // true for port 465, false for 587
   auth: {
-    user: "info@medperk.co.uk", // Must match verified sender in Brevo
+    user: "info@medperks.co.uk", // Must match verified sender in Brevo
     pass: smtpKey,
   },
 });
 
 const mailOptions = {
-  from: '"Your Name" <info@medperk.co.uk>', 
+  from: '"Your Name" <info@medperks.co.uk>', 
   to: "your-personal-email@example.com", 
   subject: "Test Email from GitHub Actions",
   text: "This is a test email sent from GitHub Actions using Brevo SMTP.",
